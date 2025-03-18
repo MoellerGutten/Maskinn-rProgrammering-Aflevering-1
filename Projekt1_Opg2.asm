c46f0c82a194d668392c0cd4ac3084561574b637
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-03-18 11:41:13 +0100

    format opg2

--- Projekt1_Opg2.asm
+++ Projekt1_Opg2.asm
@@ -3,24 +3,22 @@
 readS   ST R1, ReadS_R1 ; Calle save        
         ST R2, ReadS_R2        
         ST R3, ReadS_R3        
-    
-        
         
         AND R0, R0, #0
         AND R1, R1, #0
         AND R2, R2, #0
         AND R3, R3, #0
         
-        LEA R0, PROMPT ; Add pointer to string msg
-        PUTS ; Displays prompt
+        LEA R0, PROMPT  ; Add pointer to string msg
+        PUTS            ; Displays prompt
         
         GETC
         OUT
-        ADD R1, R0, #0 ; Move first ('x0') to R1
+        ADD R1, R0, #0  ; Move first ('x0') to R1
         
         GETC
         OUT
-        ADD R2, R0, #0 ; Move second ('0x') to R2
+        ADD R2, R0, #0  ; Move second ('0x') to R2
         
         LEA R0, newLine
         PUTS
@@ -31,15 +29,15 @@ readS   ST R1, ReadS_R1 ; Calle save
         ADD R2, R2, R3
         
         ; *10 first digit since its the 10's 
-        AND R3, R3, #0 ; Clear Counter
+        AND R3, R3, #0  ; Clear Counter
         ADD R3, R3, #10 ; Counter
-        AND R0, R0, #0 ; Accumulator
+        AND R0, R0, #0  ; Accumulator
         
-LOOP    ADD R0, R0, R1 ; Multiplying loop
+LOOP    ADD R0, R0, R1  ; Multiplying loop
         ADD R3, R3, #-1
         BRnp LOOP
         
-        ADD R0, R0, R2 ; Add second digit, the 1's
+        ADD R0, R0, R2  ; Add second digit, the 1's
         
         ; Restore registers       
         LD R1, ReadS_R1        
@@ -47,18 +45,14 @@ LOOP    ADD R0, R0, R1 ; Multiplying loop
         LD R3, ReadS_R3  
         
         RET
-        
-        
 
-
-PROMPT  .STRINGZ "Input a 2 digit decimal number: "
-newLine .STRINGZ "\n"
-CONV    .FILL #-48
+PROMPT  .STRINGZ    "Input a 2 digit decimal number: "
+newLine .STRINGZ    "\n"
+CONV    .FILL       #-48
 
 ReadS_R0    .BLKW 1 ; Space for registers
 ReadS_R1    .BLKW 1
 ReadS_R2    .BLKW 1
 ReadS_R3    .BLKW 1
-
 
         .END
