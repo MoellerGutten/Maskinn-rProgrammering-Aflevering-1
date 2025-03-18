af5db5a02249a4428f1abf8acee5bb1548a38121
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-03-18 11:39:50 +0100

    format 7.34

--- Opg7_34.asm
+++ Opg7_34.asm
@@ -2,14 +2,14 @@
         LD R0, A
         LD R1, B
         
-X       NOT R2, R0 ; (a) Negating number
-        ADD R2, R2, #1 ; (b) with 2s complement
-        ADD R2, R2, R1 ; If diff is zero we're done
-        BRz DONE ; (c) Branch to end
+X       NOT R2, R0      ; (a) Negating number
+        ADD R2, R2, #1  ; (b) with 2s complement
+        ADD R2, R2, R1  ; If diff is zero we're done
+        BRz DONE        ; (c) Branch to end
         
-        ADD R1,R1, #-1 ; Otherwise decrement big num
-        ADD R0, R0, #1 ; (d) And increment small num
-        BRnzp X ; Loop
+        ADD R1,R1, #-1  ; Otherwise decrement big num
+        ADD R0, R0, #1  ; (d) And increment small num
+        BRnzp X         ; Loop
         
 DONE    ST R1, C
         TRAP x25
